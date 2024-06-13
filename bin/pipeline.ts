b78b94e772c440103969f064b491ca48a847362f
Calvin Combs <comcalvi@amazon.com>
2024-06-13 12:57:45 -0700

    foo

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -18,4 +18,5 @@ new PipelineStack(app, 'PipelineStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  notificationArns: ['arn:aws:sns:us-east-1:131099214097:TestTopic'],
 });
